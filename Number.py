acdef09ae0b20de2e1c66db045d902186b86cc0a
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-09-02 17:21:54 +0200

    Update Number.py

--- Number.py
+++ Number.py
@@ -9,26 +9,24 @@ class number():
     def binToDec(self):
         self.dec = 0
         for i in range(len(self.bin)):
-            if self.bin[len(self.bin) - i] == '1':
+            if self.bin[len(self.bin) - i - 1] == '1':
                 self.dec += 2**i
                 
         
-    def __init__(self, decimal=None, binary=None):
-        if decimal != None or binary != None:
-            if decimal != None:
-                self.dec = decimal
-                self.decToBin()
-                return
-            if binary != None:
-                self.bin = binary
-                self.binToDec()
-                return
-        else:
-            self.bin = "0"
+    def __init__(self, input=None):
+        if input == None:
             self.dec = 0
+            self.bin = "0"
+            return
+        elif type(input) == int:
+            self.dec = input
+            self.decToBin()
+        elif type(input) == str:
+            self.bin = input
+            self.binToDec()
     
     def __str__(self):
         return "Decimal: " + str(self.dec) + "\nBinary: " + self.bin
             
-n = number(1000)
+n = number("1001011")
 print(n)
